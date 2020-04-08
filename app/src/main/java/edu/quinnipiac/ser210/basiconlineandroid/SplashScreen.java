3124faa5e0f18af3c3030a7ab682af47b552e566
meganforster <99meganforster@gmail.com>
2020-04-08 12:45:04 -0400

    Nav graph, added fragments, activities, and layout files

---
+++ app/src/main/java/edu/quinnipiac/ser210/basiconlineandroid/SplashScreen.java
@@ -0,0 +1,14 @@
+package edu.quinnipiac.ser210.basiconlineandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashScreen);
+    }
+}
