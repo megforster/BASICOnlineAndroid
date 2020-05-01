3acae1f6309ccc91b98fa5927335ab57960747dd
meganforster <99meganforster@gmail.com>
2020-05-01 09:55:26 -0400

    Progress on changebackground color

--- app/src/main/java/edu/quinnipiac/ser210/basiconlineandroid/settings.java
+++ app/src/main/java/edu/quinnipiac/ser210/basiconlineandroid/settings.java
@@ -3,10 +3,13 @@ package edu.quinnipiac.ser210.basiconlineandroid;
 
 import android.Manifest;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,12 +18,25 @@ import androidx.core.content.res.ResourcesCompat;
 
 
 public class settings extends AppCompatActivity {
-    Boolean useAlternativeTheme = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(getFlag() ? R.style.AppTheme2 : R.style.AppTheme);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Button changeColor = (Button) findViewById(R.id.colorButton);
+        changeColor.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                saveFlag(!getFlag());
+
+                Intent intent = new Intent(settings.this, settings.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     public void changeFontSize(View view) {
@@ -28,28 +44,21 @@ public class settings extends AppCompatActivity {
     }
 
     public void changeColor(View view) {
-        System.out.println("Chaning color");
-        if(useAlternativeTheme==false){
-            useAlternativeTheme = true;
-        }else{
-            useAlternativeTheme = false;
-        }
-        getTheme();
-        recreate();
         Toast.makeText(this, "In the future this will allow you to change main colors of this android app", Toast.LENGTH_SHORT).show();
     }
 
-    @Override
-    public Resources.Theme getTheme() {
-        Resources.Theme theme = super.getTheme();
-        //System.out.println(theme);
-        if(useAlternativeTheme){
-            System.out.println("Should be setting theme");
-            theme.applyStyle(R.style.AppTheme, true);
-        }else{
-            System.out.println("Setting theme back");
-            theme.applyStyle(R.style.AppTheme2, true);
-        }
-        return theme;
+    private void saveFlag(boolean flag){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean("orig", flag);
+        editor.commit();
     }
+
+    private boolean getFlag(){
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        return preferences.getBoolean("orig", false);
+    }
+
+
+
 }
